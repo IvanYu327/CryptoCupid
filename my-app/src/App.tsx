9393ee8db6fb97ee9da48f4048e54483e9bce3b3
Julian <julian.c.yam@gmail.com>
2023-06-24 16:46:47 -0400

    added world coin auth

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const prodLink = "https://id.worldcoin.org/authorize?client_id=app_c832e446eca015338033033145ce8940&response_type=code%20token%20id_token&redirect_uri=https%3A%2F%2Fgoogle.ca&state={state_value}&nonce={nonce_value}"
+
+const redirAuth = () => {
+  window.location.href = prodLink;
+};
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div>
+      testing world coin auth
     </div>
-  );
+    
+    <button onClick = {redirAuth}>
+      Sign in with world coin
+    </button>
+    </>
+  )
 }
 
 export default App;
